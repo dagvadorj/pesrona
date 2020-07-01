f6ee24ae54c7bba38e78afb63c4ecde80a7b1f0b
Dagvadorj Galbadrakh <galbadrakh@itu.edu.tr>
2020-07-02 00:39:40 +0800

    made hibernate working

---
+++ src/main/java/pesrona/model/Role.java
@@ -0,0 +1,53 @@
+package pesrona.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author dagva
+ */
+@Entity
+@Table(name = "p_role")
+public class Role implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
+    
+    private String name;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
