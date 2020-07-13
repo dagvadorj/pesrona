a9b09f0578ae6f75af9398849463a87d0a9cfb18
Dagvadorj Galbadrakh <galbadrakh@itu.edu.tr>
2020-07-13 21:09:37 +0800

    tested apis

--- src/main/java/pesrona/resources/PermissionService.java
+++ src/main/java/pesrona/resources/PermissionService.java
@@ -8,16 +8,21 @@ import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.naming.Context;
+import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
 import javax.naming.directory.DirContext;
 import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.hibernate.Session;
 import pesrona.HibernateUtil;
@@ -37,7 +42,9 @@ import pesrona.model.User;
 public class PermissionService {
 
     @POST
-    @Path("single")
+    @Path("signin")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response signin(@HeaderParam("Authorization") String authorization,
             @FormParam("username") String username, @FormParam("password") String password) {
 
@@ -62,7 +69,13 @@ public class PermissionService {
             return Response.status(400, "Bad parameter").build();
         }
 
+        System.out.println(user.getType());
+        System.out.println(user.getPassword());
+        System.out.println(password);
+        System.out.println(Hashing.sha512().hashString(password, StandardCharsets.UTF_8).toString());
+
         if (user.getType().equals("normal") && user.getPassword().equals(Hashing.sha512().hashString(password, StandardCharsets.UTF_8).toString())) {
+            System.out.println("Guten");
             session.close();
             ResponseDto responseDto = new ResponseDto();
             responseDto.setResponse(true);
@@ -79,20 +92,51 @@ public class PermissionService {
 
         Hashtable<String, String> env = new Hashtable<String, String>();
         env.put(Context.SECURITY_AUTHENTICATION, settingValues.get(NewSettingsListener.LDAP_ENCRYPTION));
-        env.put(Context.SECURITY_PRINCIPAL, "uid:" + username + "," + settingValues.get(NewSettingsListener.LDAP_BASEDN));
+        env.put(Context.SECURITY_PRINCIPAL, "uid=" + username + "," + settingValues.get(NewSettingsListener.LDAP_BASEDN));
         env.put(Context.SECURITY_CREDENTIALS, password);
 
         try {
+            System.out.println("uid:" + username + "," + settingValues.get(NewSettingsListener.LDAP_BASEDN));
+            System.out.println(password);
             env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
             env.put(Context.PROVIDER_URL, "ldap://" + settingValues.get(NewSettingsListener.LDAP_HOST) + ":" + settingValues.get(NewSettingsListener.LDAP_PORT));
             DirContext context = new InitialDirContext(env);
-            
+
+            SearchControls controls = new SearchControls();
+            controls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+//            NamingEnumeration<SearchResult> results = context.search(settingValues.get(NewSettingsListener.LDAP_BASEDN), "(" + settingValues.get(NewSettingsListener.LDAP_EMAIL) + "=*)", new Object[]{}, controls);
+//            System.out.println(results);
+//            System.out.println(results.hasMoreElements());
+//            if (!results.hasMoreElements()) {
+//                session.close();
+//                return Response.status(400, "Bad parameter").build();
+//            }
+//
+//            String loginAttribute = settingValues.get(NewSettingsListener.LDAP_LOGIN);
+//
+//            SearchResult binding = results.nextElement();
+//            System.out.println(binding);
+//            Attributes attributes = binding.getAttributes();
+//
+//            if (attributes == null) {
+//                session.close();
+//                return Response.status(400, "Bad parameter").build();
+//            }
+//
+//            if (attributes.get(loginAttribute) == null) {
+//                session.close();
+//                return Response.status(400, "Bad parameter").build();
+//            }
+//
+//            System.out.println(attributes.get(loginAttribute).get());
+
             session.close();
-            
+
             ResponseDto responseDto = new ResponseDto();
             responseDto.setResponse(true);
             return Response.ok(responseDto).build();
         } catch (NamingException e) {
+            e.printStackTrace();
             session.close();
             return Response.status(400, "Bad parameter").build();
         }
@@ -100,6 +144,8 @@ public class PermissionService {
 
     @POST
     @Path("single")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response single(@HeaderParam("Authorization") String authorization,
             @FormParam("username") String username, @FormParam("scopeCode") String scopeCode,
             @FormParam("resourceCode") String resourceCode) {
@@ -143,10 +189,15 @@ public class PermissionService {
         ResponseDto responseDto = new ResponseDto();
         responseDto.setResponse(true);
 
+        System.out.println("yoyo");
+
         return Response.ok(responseDto).build();
     }
 
     @POST
+    @Path("all")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response all(@HeaderParam("Authorization") String authorization,
             @FormParam("username") String username) {
 
@@ -154,7 +205,7 @@ public class PermissionService {
             return Response.status(400, "Token is required").build();
         }
 
-        String token = authorization.substring("Basic".length()).trim();
+        String token = authorization.substring("Bearer".length()).trim();
 
         Session session = HibernateUtil.getSessionFactory().openSession();
         Client client = (Client) session.createQuery("select o from Client o where o.token = :token").
@@ -171,17 +222,29 @@ public class PermissionService {
             return Response.status(400, "Bad parameter").build();
         }
 
-        List<PermissionDto> permissionDtos = session.createQuery("select o.scope.code as scopeCode, o.scope.name as scope, o1.resource.code as resourceCode, o1.resource.name as resource from Permission o, Assignment o1 where o.role = o1.role and o.client = :client and o1.user = :user")
+        List<Object[]> permissions = session.createQuery("select o.scope.code as scopeCode, o.scope.name as scope, o1.resource.code as resourceCode, o1.resource.name as resource from Permission o, Assignment o1 where o.role = o1.role and o.client = :client and o1.user = :user")
                 .setParameter("client", client)
                 .setParameter("user", user)
                 .getResultList();
-        if (permissionDtos.isEmpty()) {
+        if (permissions.isEmpty()) {
             session.close();
             return Response.status(400, "No access").build();
         }
 
         session.close();
 
+        List<PermissionDto> permissionDtos = new ArrayList<>();
+
+        for (Object[] object : permissions) {
+            PermissionDto permissionDto = new PermissionDto();
+            permissionDto.setScopeCode((String) object[0]);
+            permissionDto.setScope((String) object[1]);
+            permissionDto.setResourceCode((String) object[2]);
+            permissionDto.setResource((String) object[3]);
+            permissionDtos.add(permissionDto);
+        }
+
+        System.out.println(permissionDtos);
         return Response.ok(permissionDtos).build();
     }
 
